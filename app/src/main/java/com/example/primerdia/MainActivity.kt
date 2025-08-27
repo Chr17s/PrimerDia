4f8ba8fa1b15753ea61f4170fbde42a47a697aa3
Chr17s <crishdz690@gmail.com>
2025-08-27 13:12:17 -0600

    INICIO

--- app/src/main/java/com/example/primerdia/MainActivity.kt
+++ app/src/main/java/com/example/primerdia/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                 //hola
                 //hola2
                 //22130813
+                //prueba
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
